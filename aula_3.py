671997028b7389e95f0f25e4b0d9ad6b9c26e9df
giordanogomes <gfgomes16@gmail.com>
2022-02-16 17:28:28 -0300

    add multiplicaçao

--- aula_3.py
+++ aula_3.py
@@ -6,11 +6,11 @@ def soma(a, b):
     print(f'{a} + {b} = {a + b}')
 
 
-def divisao(a, b):
-    print(f'{a} / {b} = {a / b}')
+def multiplica(a, b):
+    print(f'{a} x {b} = {a * b}')
     
 
 
 divide(20, 5)
 soma(30, 10)
-divisao(50, 5)
+multiplica(10, 5)
